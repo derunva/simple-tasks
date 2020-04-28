00a6be1d99f25d09293f71cbc55f5439e6c66e4a
derunva <derun.va@gmail.com>
2020-04-28 14:16:52 +0300

    http axios settings & vuex store settings

--- app/javascript/packs/tasks.js
+++ app/javascript/packs/tasks.js
@@ -1,10 +1,20 @@
 import Vue from 'vue'
 import App from '../app.vue'
+import axios from 'axios'
+import store from '../store'
+
+Vue.prototype.$axios = axios
+Vue.prototype.$axios.defaults.headers.common = {
+    'X-Requested-With': 'XMLHttpRequest',
+    'X-CSRF-TOKEN' : document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+}
+Vue.prototype.$axios.defaults.baseURL = '/'
 
 document.addEventListener('DOMContentLoaded', () => {
   const el = document.querySelector('.application').appendChild(document.createElement('application'))
   const app = new Vue({
     el,
+    store,
     render: h => h(App)
   })
   console.log(app)
